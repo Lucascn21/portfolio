65c28d78f5e8fa56d82af91918d7764750578f67
lucascn21 <lucascn21@gmail.com>
2023-04-18 13:29:36 -0300

    mocking

--- src/App.js
+++ src/App.js
@@ -12,7 +12,28 @@ function App() {
         </nav>
       </aside>
       <main>
-        <section id="landing">landing</section>
+        <section id="landing">
+          <iframe
+            title="test"
+            src="https://giphy.com/embed/XEyg6fn0RAYH76efh7"
+            className="giphy-embed"
+            allowFullScreen
+          ></iframe>
+
+          <h1>Lucas Goldental</h1>
+          <p>
+            Student Dev <span>Problem Solver</span>
+          </p>
+          <p id="descriptionLanding">
+            My name is Lucas, i am a<span id="landingAsideText"></span> from
+            Argentina.
+            <br />
+            According to some function in scripts.js, i am
+            <span id="landingAgeText"></span> years old. I am currently diving
+            into Coding. I have also dabbled in Game Development and Human
+            Resources. My goal and focus is becoming a Javascript Developer.
+          </p>
+        </section>
         <section id="projects">projects</section>
         <section id="skills">skills</section>
         <section id="contact">contact</section>
