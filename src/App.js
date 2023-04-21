b9db3cb5a11ef69f60524491bbd0c368f4e865bc
lucascn21 <lucascn21@gmail.com>
2023-04-21 19:56:46 -0300

    lots of mocking

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ function App() {
         <section id="landing">
           <article id="landingTextArea">
             <h1>Lucas Goldental</h1>
-            <p>
+            <p className="italic">
               Student Dev
-              <span>Problem Solver</span>
+              <span>&nbsp;Problem Solver</span>
             </p>
             <p id="descriptionLanding">
-              My name is Lucas, i am a
+              A&nbsp;
               <span id="landingAsideText">
                 <span className="Typewriter__wrapper">
                   Selftaught C1 English User
                 </span>
                 <span className="Typewriter__cursor">|</span>
               </span>
-              from Argentina. According to some function in scripts.js, i am
-              <span id="landingAgeText"> 30</span> years old. I am currently
-              diving into Coding. I have also dabbled in Game Development and
-              Human Resources. My goal and focus is becoming a Javascript
-              Developer.
+              &nbsp;from Argentina. According to some function the app, i am
+              <span id="landingAgeText"> 30</span> years old.
+              <br />
+              I am currently diving into coding. I have also dabbled in Game
+              Development and Human Resources.
+              <br />
+              My goal and focus is becoming a Javascript Developer.
             </p>
           </article>
           <article id="gifArea">
@@ -89,12 +91,55 @@ function App() {
           </article>
         </section>
         <section id="about">
-          <article id="aboutMe">
-            <h2>asdadasddsada</h2>
-            <p>
-              asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdaasdasdasdadssadadadsadasdsadsadsadsdasdasdasdasdasdasdasdasdasdaasdasdasdadssadadadsadasdsadsadsadsdasdasdasdasdasdasdasdasdasdaasdasdasdadssadadadsadasdsadsadsadsdasdasdasdasdaasdasdasdadssadadadsadasdsadsadsad
-            </p>
-          </article>
+          <h2>About me</h2>
+          <h3> Childhood</h3>
+          <p>
+            I was born on August, 1992. i've learned to read quite early, both
+            english and spanish and i've been around computers most of my life.
+            My dad was my main influence, i used to sit next to him sometimes
+            while he worked and i picked up some basics, he also used to teach
+            or explain anything i asked. That was me, learning computer basics
+            and english along the way, by means of games, programs and my dad.
+          </p>
+          <h3>Adolescence</h3>
+          <p>
+            This is when i first approached to coding
+            <span className="italic">(C++ & SQL)</span>, while learning how to
+            use game server frameworks. I've also taken a deeper interest in
+            hardware and eventually built my first custom pc.
+          </p>
+          <h3>Adulthood</h3>
+          <p>
+            <span className="italic">
+              This is where i realized and decided that i am going to be a
+              developer.&nbsp;
+            </span>
+            I got a part-time position into the Human Resources department, as a
+            Consultant. I took part in modernizing the department's practices.
+            Albeit against some resistance to change.
+            <br />
+            &nbsp;Not losing in sight that this is a job i like and do well but
+            its not a job for me to build my carreer on, i've been focusing and
+            investing in this one definitive goal that is becoming a developer.
+            <h4>To summarize who i am and what have i achieved recently:</h4>
+            <span className="bolder">
+              <p>
+                I am constantly learning and currently pursuing an IT Degree.
+                <br />
+                I know many languages and technologies. I am capable of picking
+                up new ones.
+                <br />
+                I got the curiosity and resourcefulness that i believe are key
+                towards learning and improvement.
+                <br />
+                I am currently learning React.
+                <br />
+                I have very good analytical abilites, i really enjoy solving
+                problems and helping others.
+                <br /> I want to be part in the creation of useful things.
+              </p>
+            </span>
+          </p>
         </section>
       </main>
       <footer>footer</footer>
