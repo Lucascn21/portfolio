5b3be36399d4527f59fe9d1df04a39a329d827bd
lucascn21 <lucascn21@gmail.com>
2023-04-26 17:49:53 -0300

    Modelling data

---
+++ src/data/data.js
@@ -0,0 +1,122 @@
+const Projects = [
+  {
+    name: "Project 1",
+    tech: ["JS", "HTML", "CSS3"],
+    reason: "A reason",
+    things: {
+      learned: ["better this", "how to that", "the reason for"],
+      toImprove: [
+        "Better layout and responsive practices with grid",
+        "Project folder structuring",
+      ],
+      toDo: [
+        "Remake with a better overall theme specification design system and ideas",
+      ],
+    },
+    takeAway:
+      "I remade a nodejs portfolio in react, my code improved vastly, i better understand the pros and cons of React and responsiveness. ",
+    Links: {
+      Github: "https://github.com/link",
+      Vercel: "https://vercel.com/link",
+      Glitch: "https://glitch.com/link",
+    },
+  },
+  {
+    tech: ["JS", "HTML", "CSS3"],
+    reason: "A reason 2",
+    things: {
+      learned: ["better this 2", "how to that 2", "the reason for 2"],
+      toImprove: [
+        "Better layout and responsive practices with grid 2",
+        "Project folder structuring 2",
+      ],
+      toDo: [
+        "Remake with a better overall theme specification design system and ideas 2",
+      ],
+    },
+    takeAway:
+      "I remade a nodejs portfolio in react, my code improved vastly, i better understand the pros and cons of React and responsiveness. 2",
+    Links: {
+      Github: "https://github.com/link2",
+      Vercel: "https://vercel.com/link2",
+      Glitch: "https://glitch.com/link2",
+    },
+  },
+];
+
+const Courses = [
+  {
+    ID: 1,
+    name: "Course 1",
+    institution: "Codecademy",
+    tech: ["JS", "HTML", "CSS3"],
+    things: {
+      learned: ["better this", "how to that", "the reason for"],
+      achieved: ["coding knowledge", "a project to show"],
+      toDo: [
+        "Remake with a better overall theme specification design system and ideas",
+      ],
+    },
+    takeAway: "I learned how to code ",
+  },
+  {
+    ID: 2,
+    name: "course 2",
+    institution: "codecademy 2",
+    tech: ["JS2", "HTML2", "CSS32"],
+    things: {
+      learned: ["better this 2", "how to that 2", "the reason for 2"],
+      achieved: ["coding knowledge 2", "a project to show 2"],
+      toDo: [
+        "Remake with a better overall theme specification design system and ideas 2",
+      ],
+    },
+    takeAway: "I learned how to code 2",
+  },  {
+    ID: 3,
+    name: "course 3",
+    institution: "codecademy 3",
+    tech: ["JS3", "HTML3", "CSS33"],
+    things: {
+      learned: ["better this 3", "how to that 3", "the reason for 3"],
+      achieved: ["coding knowledge 3", "a project to show 3"],
+      toDo: [
+        "Remake with a better overall theme specification design system and ideas 3",
+      ],
+    },
+    takeAway: "I learned how to code 2",
+  },
+];
+
+const Tecnologies = [
+  { Name: "JS", Type: "Lang" },
+  { Name: "React", Type: "Lib" },
+  {
+    Name: "ExpressJs",
+    Type: "Framework",
+  },
+  {
+    Name: "Vercel",
+    Type: "Cloud",
+  },
+  {
+    Name: "PWA",
+    Type: "Principles",
+  },
+  {
+    Name: "Figma",
+    Type: "WebDev",
+  },
+  {
+    Name: "MongoDB",
+    Type: "DBMS",
+  },
+  {
+    Name: "Sequelize",
+    Type: "ORM",
+  },
+  {
+    Name: "Git",
+    Type: "Project Management",
+  },
+];
