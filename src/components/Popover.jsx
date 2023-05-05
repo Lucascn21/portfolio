4b6b8c1284381890bca2db41e14563aff2dfdfd1
lucascn21 <lucascn21@gmail.com>
2023-05-05 17:10:04 -0300

    modal functionality testing

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -5,28 +5,33 @@ export const Popover = ({ modalData, handleModal }) => {
   console.dir(modalData);
 
   return (
-    <article onClick={() => handleModal()} id="popover">
-      <section id="modalHeader">
-        <h2>{modalData.Name}</h2>
-        <h3>{modalData.Tech.map((tech) => ` ${tech} `)}</h3>
-        <p>{modalData.Institution || modalData.Reason}</p>
-      </section>
-      <section id="things">
-        <h2>Things</h2>
-        {modalData.Things.Achieved ? (
-          <List section="Achieved" listData={modalData.Things.Achieved} />
-        ) : (
-          <List section="Toimprove" listData={modalData.Things.ToImprove} />
-        )}
+    <>
+      <div id="popoverBg" onClick={() => handleModal()}>
+        <p id="popoverBgText">Click/Tap here to go back</p>
+      </div>
+      <article id="popover" className="requires-no-scroll">
+        <section id="modalHeader">
+          <h2>{modalData.Name}</h2>
+          <h3>{modalData.Tech.map((tech) => ` ${tech} `)}</h3>
+          <p>{modalData.Institution || modalData.Reason}</p>
+        </section>
+        <section id="things">
+          <h2>Things</h2>
+          {modalData.Things.Achieved ? (
+            <List section="Achieved" listData={modalData.Things.Achieved} />
+          ) : (
+            <List section="Toimprove" listData={modalData.Things.ToImprove} />
+          )}
 
-        <List section="Learned" listData={modalData.Things.Learned} />
+          <List section="Learned" listData={modalData.Things.Learned} />
 
-        <List section="ToDo" listData={modalData.Things.ToDo} />
-      </section>
-      <section id="takeaway">
-        <h2>Take Away</h2>
-        <p>{modalData.TakeAway}</p>
-      </section>
-    </article>
+          <List section="ToDo" listData={modalData.Things.ToDo} />
+        </section>
+        <section id="takeaway">
+          <h2>Take Away</h2>
+          <p>{modalData.TakeAway}</p>
+        </section>
+      </article>
+    </>
   );
 };
