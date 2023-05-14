9fdf4ae09c4d7cfd6c68bea25a49f380926f6da3
lucascn21 <lucascn21@gmail.com>
2023-05-14 16:38:48 -0300

    In progress skill behaviour

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-export const Button = ({ text, onClick, gradient }) => {
+export const Button = ({ text, onClick, gradient, isInProgress }) => {
   return (
     <button
       style={{
         background: `linear-gradient(90deg, rgb(37, 37, 48) 0%,rgb(253, 194, 29) ${
           gradient * 0.33
         }%, rgb(4 170 109) ${gradient}%, rgb(37, 37, 48) ${gradient}%)`,
+        opacity: isInProgress ? 0.5 : 1,
+        pointerEvents: isInProgress && "none",
       }}
       onClick={onClick}
+      disabled={isInProgress}
     >
       {text}
     </button>
